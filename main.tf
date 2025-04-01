8f2f47f32da8f772e01ecc3c3c46a01289f448b3
sujeet kumar kushwaha <sujeetkushwaha14@gmail.com>
2025-04-02 04:13:12 +0530

    second commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "us-east-1"  # AWS region
+}
+
+resource "aws_instance" "hello_world" {
+  ami           = "ami-002f6e91abff6eb96"  # Example Amazon Linux AMI
+  instance_type = "t2.micro"
+
+  # User data script to create a simple Hello World page
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "<html><body><h1>Hello World from Terraform!</h1></body></html>" > /var/www/html/index.html
+              service httpd start
+              chkconfig httpd on
+              EOF
+
+  tags = {
+    Name = "HelloWorldServer"
+  }
+}
+
+
+
+output "instance_public_ip" {
+  value = aws_instance.hello_world.public_ip
+}
+
